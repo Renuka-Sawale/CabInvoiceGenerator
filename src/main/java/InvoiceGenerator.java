5533373d059191fc974d815e9f8c6928a3ae2108
Renuka Sawale <rrsuryawanshi11@gmail.com>
2021-05-24 01:37:55 +0530

    Added Cab invoice generator Total Fare

--- src/main/java/InvoiceGenerator.java
+++ src/main/java/InvoiceGenerator.java
@@ -1,11 +1,8 @@
 public class InvoiceGenerator {
-    /*private static final int COST_PER_TIME = 1;
+    private static final int COST_PER_TIME = 1;
     private static final double MINIMUM_COST_PER_KILOMETER = 10;
 
     public double calculateFare(double distance, int time) {
         return distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
-    }*/
-    public static void main(String[] args) {
-        System.out.println("Cab invoice generator");
     }
 }
